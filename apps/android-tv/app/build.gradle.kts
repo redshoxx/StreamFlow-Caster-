a62d46d288802ceef6c524c8258eb9fba17affca
redshoxx <48377562+redshoxx@users.noreply.github.com>
2026-08-16 15:52:55 +0200

    Align StreamFlow TV with Universal Casting 0.7

--- apps/android-tv/app/build.gradle.kts
+++ apps/android-tv/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.redshoxx.streamflow.tv"
         minSdk = 26
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.6.0"
+        versionCode = 8
+        versionName = "0.7.0"
     }
 
     buildFeatures { compose = true }
